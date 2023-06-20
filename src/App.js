c36fe4bf102c959702731c34b2004cc4f5ddafeb
patelrudy <100442399+patelrudy@users.noreply.github.com>
2023-06-20 17:36:54 -0400

    about me page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import NavBar from './components/NavBar';
 import './App.css';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} /> {/* add this */}
+          {/* Add additional Routes for other pages here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import './NavBar.css';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isOpen]);
+
+  return (
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <ul className={`navbar-menu ${isOpen ? 'open' : ''}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/resume">Resume</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li className="navbar-icons">
+          <a href="https://github.com/patelrudy" target="_blank" rel="noopener noreferrer" className="navbar-icon">
+            <FaGithub size={20}/>
+          </a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="navbar-icon">
+            <FaLinkedin size={20}/>
+          </a>
+          <a href="mailto:patelrudy@yahoo.com" className="navbar-icon">
+            <FaEnvelope size={20}/>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
